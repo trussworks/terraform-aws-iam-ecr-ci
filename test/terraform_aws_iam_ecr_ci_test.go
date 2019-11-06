60f59bd2fc773a2ee8b3c1722787946edab5c9eb
kilbergr <kilbergr@users.noreply.github.com>
2019-11-06 13:46:10 -0800

    add AWS_REGION default

--- test/terraform_aws_iam_ecr_ci_test.go
+++ test/terraform_aws_iam_ecr_ci_test.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
