0dc605115cef397aca0d7104e89b7fa2cb1b97db
kilbergr <kilbergr@users.noreply.github.com>
2019-11-07 11:30:54 -0800

    update example to tf12

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,10 +1,10 @@
 module "ecr_ci" {
   source = "../../"
 
-  ecr_repo = "${aws_ecr_repository.main.name}"
-  ci_name  = "${var.ci_name}"
+  ecr_repo = aws_ecr_repository.main.name
+  ci_name  = var.ci_name
 }
 
 resource "aws_ecr_repository" "main" {
-  name = "${var.ecr_repo}"
+  name = var.ecr_repo
 }
